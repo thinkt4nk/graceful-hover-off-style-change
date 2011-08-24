46e614290f6e45988e709fa12908f00d7455a81a
Ryan Bales <ryanbales@Ryan-Baless-iMac.local>
2011-08-24 11:59:43 -0500

    added support for tick callback, animate styles and interval, transition handlers, etc

--- jquery.style-timer.js
+++ jquery.style-timer.js
@@ -17,10 +17,48 @@
 		jNode.removeData('interval_id');
 	};
 
-	var applyStyle = function(jNode,style) {
+	var defineStyle = function(jNode,style,options) {
+			$(Object.keys(style)).each(function() {
+				jNode.css(''+this,style[''+this]);
+			});
+	}
+
+	var stripAnimate = function(style)
+	{
+		var return_object = {};
 		$(Object.keys(style)).each(function() {
-			jNode.css(''+this,style[''+this]);
+			if( ''+this !== 'animate_style' )
+			{
+				return_object[''+this] = style[''+this];
+			}
 		});
+		return return_object;
+	};
+
+	var applyStyle = function(jNode,style,options) {
+		if( options.animate !== false )
+		{
+			var animate_style = style
+				,animate_callback = function() {};
+			if( typeof(style.animate_style) !== 'undefined' ) {
+				animate_style = style.animate_style;
+				animate_callback = function() {
+					defineStyle(jNode,stripAnimate(style),options);
+				}
+			}
+			jNode.animate(animate_style,options.animate_interval,animate_callback);
+		} else {
+			defineStyle.apply(null,arguments);
+		}
+	};
+
+	var applyTransition = function(jNode,transition,options) {
+		if( typeof(transition) === 'function' )
+		{
+			transition.apply(null,arguments);
+		} else if( Object.keys(transition).length > 0 ) {
+			applyStyle.apply(null,arguments);
+		}
 	};
 
 	$.fn.hoverStyleTimer = function(options) {
@@ -29,6 +67,10 @@
 			timeAfterHoverOff : 5,		// the padding time after hover off
 			onHoverOn : {},				// style must be set
 			onHoverOff : {},			// style must be set
+			animate : false,			// animate styles
+			animate_interval : 'slow',	// takes any valid animate interval argument
+			onTransition : {},			// transitional style or handler
+			onTick : {}					// tick style or handler
 		};
 
 		var options = $.extend({},defaults,options);
@@ -37,7 +79,7 @@
 			$(this).hover(
 				function() {
 					resetStyleTimer($(this));
-					applyStyle($(this),options.onHoverOn);
+					applyTransition($(this),options.onHoverOn,options);
 				},
 				function() {
 					if( typeof($(this).data('hide_timer')) === 'undefined' )
@@ -49,14 +91,17 @@
 							{
 								// tick
 								$(this).data('hide_timer',timer_tick - 1);
+								applyTransition($(this),options.onTick,options);
 							} else {
 								resetStyleTimer($(this));
 								// reset style
-								applyStyle($(this),options.onHoverOff);
+								applyTransition($(this),options.onHoverOff,options);
 							}
 						}.bind(this),1000));
 						// set timer
 						$(this).data('hide_timer',options.timeAfterHoverOff);
+						// apply transitional style/callback
+						applyTransition($(this),options.onTransition,options);
 					}
 				}
 			);
